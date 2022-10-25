4c7456db0d522d16ea459e16129629deeb03c360
Dave Reed <david.reed.c@gmail.com>
2022-10-24 21:27:28 -0400

    added color to bracket

--- src/Bracket.tsx
+++ src/Bracket.tsx
@@ -4,9 +4,10 @@ interface IProps {
   width: number;
   height: number;
   label: string;
+  color?: string;
 }
 
-export const Bracket = ({ width, height, label }: IProps) => {
+export const Bracket = ({ width, height, label, color = "#aaa" }: IProps) => {
   const path = d3.path();
 
   path.moveTo(0, 0);
@@ -16,13 +17,13 @@ export const Bracket = ({ width, height, label }: IProps) => {
 
   return (
     <g>
-      <path d={path.toString()} stroke="black" strokeWidth={1} fill="none" />
+      <path d={path.toString()} stroke={color} strokeWidth={1} fill="none" />
       <line
         x1={width}
         x2={width + 5}
         y1={height / 2}
         y2={height / 2}
-        stroke="black"
+        stroke={color}
         strokeWidth={1}
       />
       <text
@@ -30,6 +31,7 @@ export const Bracket = ({ width, height, label }: IProps) => {
         y={height / 2}
         fontSize={10}
         alignmentBaseline="middle"
+        fill={color}
       >
         {label}
       </text>
