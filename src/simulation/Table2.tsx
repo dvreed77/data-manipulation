0ca67c0348f7c1f108ec40ed3d4f9eb9f5704988
Dave Reed <dave.reed@alteryx.com>
2022-10-28 20:00:31 -0400

    sticky tables are better

--- src/simulation/Table2.tsx
+++ src/simulation/Table2.tsx
@@ -17,7 +17,7 @@ const TableRows = ({ data }: { data: Row[] }) => {
       {data.map((row) => (
         <tr
           key={row.key}
-          className={`cursor-pointer ${myRow === row.key ? "bg-gray-200" : ""}`}
+          className={`table-row ${myRow === row.key ? "bg-gray-200" : ""}`}
         >
           <td
             id={`row-${row.key}`}
@@ -39,32 +39,34 @@ const TableRows = ({ data }: { data: Row[] }) => {
 
 export const Table2 = ({ data }: { data: Row[] }) => {
   return (
-    <table className="min-w-full divide-y divide-gray-300">
-      <thead className="bg-gray-50 block">
-        <tr>
-          <th
-            scope="col"
-            className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
-          >
-            #
-          </th>
-          <th
-            scope="col"
-            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-          >
-            Date
-          </th>
-          <th
-            scope="col"
-            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-          >
-            Sales
-          </th>
-        </tr>
-      </thead>
-      <tbody className="divide-y divide-gray-200 bg-white  max-h-96 overflow-auto block">
-        <TableRows data={data} />
-      </tbody>
-    </table>
+    <div className="overflow-x-auto max-h-96">
+      <table className="min-w-full">
+        <thead className="bg-gray-50 table-header-group sticky top-0 border-b border-gray-400 backdrop-blur backdrop-filter bg-opacity-75">
+          <tr>
+            <th
+              scope="col"
+              className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+            >
+              #
+            </th>
+            <th
+              scope="col"
+              className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+            >
+              Date
+            </th>
+            <th
+              scope="col"
+              className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+            >
+              Sales
+            </th>
+          </tr>
+        </thead>
+        <tbody className="divide-y divide-gray-200 bg-white  max-h-96 overflow-auto table-row-group">
+          <TableRows data={data} />
+        </tbody>
+      </table>
+    </div>
   );
 };
